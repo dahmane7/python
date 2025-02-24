11000d491403929d934b5350bfb4ab631a172405
dahmane7 <bilelzineb@gmail.com>
2025-02-24 15:19:02 +0100

    Add files via upload

---
+++ Python/exercice.py
@@ -0,0 +1,51 @@
+# Exercice 1 – Afficher des messages
+# print("Bonjour, bienvenue dans le cours de Python !")
+# Exercice 2 – Calculatrice simple
+# a = input("Taper la valeur de a : ")
+# b = input("Taper la valeur de b : ")
+# somme = int(a) + int(b)
+# print("la somme de a et b est : ", somme)
+# nombre1 = float(input("premier nombre:"))
+# nombre2 = int(input("second nombre:"))
+# Exercice 3 – Pair ou Impair
+# n = int(input("Taper le nombre de valeurs : "))
+# if (n%2 == 0):
+#     print ("le nombre est pair")
+# else:
+#     print ("le nombre est impair")
+# Exercice 4 – Plus grand que 10
+# nombre=int(input("Taper le nombre : "))
+# if nombre > 10:
+#     print("le nombre est plus grand que 10")
+# else:
+#     print("le nombre est plus petit que 10")
+# annee de naissance
+# annee_naissance = int(input("Taper l'annee de naissance : "))
+# age=2025-annee_naissance
+# print(f"Vous avez {age} : , ans.")
+# Comparaison de deux nombres
+# nombre1 = int(input("Taper le premier nombre : "))
+# nombre2 = int(input("Taper le second nombre : "))
+# if nombre1 > nombre2:
+#     print("le premier nombre est plus grand que le second")
+# elif  nombre2> nombre1:
+#     print("le deuxieme nombre est plus grand que le premier")
+# else:
+#     print("les deux nombres sont egaux")
+# Calculer l’aire d’un rectangle
+# 
+#  Moyenne de trois notes
+note1=float(input("Taper la premiere note : "))
+note2=float(input("Taper la seconde note : "))
+note3=float(input("Taper la derniere note : "))
+moyenne=(note1+note2+note3)/3
+print(f"La moyenne des notes est de {moyenne}")
+if moyenne >= 10:
+    print("Admis")
+    print(f"Admis {moyenne}")
+else:
+    print("Refusé")
+
+
+
+
